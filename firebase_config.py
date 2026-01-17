4cf8e995fa95b528456219957a1fc178171b2224
aaqlh <ainaaaqilahh@gmail.com>
2026-01-18 06:26:11 +0800

    Fix Firebase key PEM issue for Render

--- firebase_config.py
+++ firebase_config.py
@@ -1,6 +1,6 @@
 import os
 import firebase_admin
-from firebase_admin import credentials
+from firebase_admin import credentials, firestore
 
 cred = credentials.Certificate({
     "type": os.environ.get("FIREBASE_TYPE"),
@@ -9,10 +9,11 @@ cred = credentials.Certificate({
     "private_key": os.environ.get("FIREBASE_PRIVATE_KEY").replace("\\n", "\n"),
     "client_email": os.environ.get("FIREBASE_CLIENT_EMAIL"),
     "client_id": os.environ.get("FIREBASE_CLIENT_ID"),
-    "auth_uri": os.environ.get("FIREBASE_AUTH_URI"),
-    "token_uri": os.environ.get("FIREBASE_TOKEN_URI"),
-    "auth_provider_x509_cert_url": os.environ.get("FIREBASE_AUTH_PROVIDER_CERT_URL"),
+    "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+    "token_uri": "https://oauth2.googleapis.com/token",
+    "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
     "client_x509_cert_url": os.environ.get("FIREBASE_CLIENT_CERT_URL")
 })
 
 firebase_admin.initialize_app(cred)
+db = firestore.client()
