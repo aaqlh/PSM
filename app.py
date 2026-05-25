e8830edb42c14c072c7b67bf465ab6617db97ef8
aaqlh <ainaaaqilahh@gmail.com>
2026-05-25 10:15:19 +0800

    correct google verification

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
-from flask import Flask
+from flask import Flask, send_file
 from controllers.register_login_controller import auth
 from controllers.account_profile_controller import account_profile
 from controllers.expense_controller import expense_bp
 from controllers.savings_goal_controller import savings_bp
-from controllers.ai_recommendation_controller import ai  # pastikan ini nama blueprint betul
-from flask import send_file
+from controllers.ai_recommendation_controller import ai
 
 # Firebase handled in firebase_config.py
-from firebase_config import db  
+from firebase_config import db
+
+app = Flask(__name__)
+app.secret_key = "super-secret-key-123"
+
+# Google verification route
 @app.route('/google123456789.html')
 def google_verify():
     return send_file('google123456789.html')
-app = Flask(__name__)
-app.secret_key = "super-secret-key-123"
 
 # Register blueprints
 app.register_blueprint(auth)
@@ -24,4 +26,4 @@ app.register_blueprint(ai)
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, debug=False)  # Render fix
+    app.run(host="0.0.0.0", port=port, debug=False)
